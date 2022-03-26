9269b40d5b38a0e170ad3247e6ed9b7af941e5cd
neven kusturica <neven.kusturica@gmail.com>
2022-03-26 21:55:45 +0100

    dodao nesta

---
+++ myScript.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    document.getElementById("demo").style.color = "red";
+}
+
+console.log('hello world');
+
+
+let x = 3;
+let b = 4;
+let z = x + b;
+console.log('rezultat je ' + z);
+
+document.getElementById("g").innerHTML = z;
+
+
+window.alert("tekst");
+
+let xc = 10;
+let y = 20;
+let t = x % y;
+
+document.getElementById("t").innerHTML = t;
+
+function disableElement() { document.getElementById("btn01").disabled = true; }
+
+
